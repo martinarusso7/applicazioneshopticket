252a1aa59a77526dff2f55c8661039aee9df389d
martinarusso7 <martinarusso.1997.mr@gmail.com>
2022-10-04 09:27:19 +0200

    keycloak_modificacodice

--- src/main/java/ShopTicket/controller/HomeController.java
+++ src/main/java/ShopTicket/controller/HomeController.java
@@ -1,17 +1,36 @@
 package ShopTicket.controller;
 
 
+import org.keycloak.KeycloakSecurityContext;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Set;
 
 @Controller
 public class HomeController {
 
+        private KeycloakSecurityContext getSecurityContext() {
+        final HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        return (KeycloakSecurityContext) request.getAttribute(KeycloakSecurityContext.class.getName());
+        }
+
         @GetMapping("/login")
         public String login() {
-            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+            Set<String> ruolo = getSecurityContext().getToken().getRealmAccess().getRoles();
+            if (ruolo.contains("Admin")){
+                return "redirect:/home-admin";
+            }
+
+            return "redirect:/homepage";
+
+            /*Authentication auth = SecurityContextHolder.getContext().getAuthentication();
             if (auth != null && auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ADMIN"))) {
 
                 return "redirect:/home-admin";
@@ -21,12 +40,20 @@ public class HomeController {
                 return "redirect:/homepage";
             }
             else
-                return "login";
+                return "login";*/
         }
 
         @GetMapping("/")
         public String homepage() {
-            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+            Set<String> ruolo = getSecurityContext().getToken().getRealmAccess().getRoles();
+            if (ruolo.contains("Admin")){
+                return "redirect:/home-admin";
+            }
+
+            return "redirect:/homepage";
+
+            /*Authentication auth = SecurityContextHolder.getContext().getAuthentication();
             if (auth != null && auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ADMIN"))) {
 
                 return "redirect:/home-admin";
@@ -36,7 +63,7 @@ public class HomeController {
                 return "redirect:/homepage";
             }
             else
-                return "login";
+                return "login";*/
         }
 
 
